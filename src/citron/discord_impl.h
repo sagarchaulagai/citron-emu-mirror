3eec1ced93cc9efe77ec7d22a5664e9250758a69
Zephyron <zephyron@citron-emu.org>
2025-05-06 16:42:30 +1000

    feat(discord): Update Discord integration and game art fetching
    - Updates the Discord application Client ID.
    - Changes the game art source from static URLs on `citron-emu.org` to dynamically fetched images from `tinfoil.media` using the game's title ID.
    - Removes the `GetGameString` function, as the URL formatting logic has changed.
    - Modifies `UpdateGameStatus` and `Update` to accommodate the new image fetching mechanism and use the title ID.
    - Adds new members to `DiscordImpl` to store title ID and cache image URLs.
    - Updates copyright information in `discord_impl.cpp` and `discord_impl.h`.
    
    Signed-off-by: Zephyron <zephyron@citron-emu.org>

--- src/citron/discord_impl.h
+++ src/citron/discord_impl.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2018 Citra Emulator Project
+// SPDX-FileCopyrightText: 2025 citron Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
@@ -11,22 +12,22 @@ class System;
 
 namespace DiscordRPC {
 
-class DiscordImpl : public DiscordInterface {
-public:
-    DiscordImpl(Core::System& system_);
-    ~DiscordImpl() override;
+class DiscordImpl: public DiscordInterface {
+public: DiscordImpl(Core::System & system_);
+~DiscordImpl() override;
 
-    void Pause() override;
-    void Update() override;
+void Pause() override;
+void Update() override;
 
-private:
-    std::string GetGameString(const std::string& title);
-    void UpdateGameStatus(bool use_default);
+private: void UpdateGameStatus(bool use_default);
 
-    std::string game_url{};
-    std::string game_title{};
+std::string game_url {};
+std::string game_title {};
+std::string game_title_id {};
+std::string cached_url;
 
-    Core::System& system;
+Core::System & system;
+u64 program_id = 0;
 };
 
 } // namespace DiscordRPC
