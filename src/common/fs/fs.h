8ee84ee519a385b9edc2ad1e00cf8881d8004beb
Collecting <collecting@noreply.localhost>
2025-12-19 18:40:19 +0000

    fs: Fix Directory Scanning w/ NTFS
    Signed-off-by: Collecting <collecting@noreply.localhost>

--- src/common/fs/fs.h
+++ src/common/fs/fs.h
@@ -399,6 +399,10 @@ void IterateDirEntriesRecursively(const std::filesystem::path& path,
                                   const DirEntryCallable& callback,
                                   DirEntryFilter filter = DirEntryFilter::All);
 
+void IterateDirEntriesRecursivelyInternal(const std::filesystem::path& path,
+                                          const DirEntryCallable& callback,
+                                          DirEntryFilter filter, int depth);
+
 #ifdef _WIN32
 template <typename Path>
 void IterateDirEntriesRecursively(const Path& path, const DirEntryCallable& callback,
