9c49ec0e97b21bd025edd4f54f84fef149a50a61
Collecting <collecting@noreply.localhost>
2026-01-05 10:29:41 +0000

    fix(gamescope): Restructure Multiplayer Dialogs for Gamescope
    Signed-off-by: Collecting <collecting@noreply.localhost>

--- src/citron/multiplayer/host_room.cpp
+++ src/citron/multiplayer/host_room.cpp
@@ -32,10 +32,23 @@
 HostRoomWindow::HostRoomWindow(QWidget* parent, QStandardItemModel* list,
                                std::shared_ptr<Core::AnnounceMultiplayerSession> session,
                                Core::System& system_)
-    : QDialog(parent, Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::WindowSystemMenuHint),
+    : QDialog(parent),
       ui(std::make_unique<Ui::HostRoom>()),
       announce_multiplayer_session(session), system{system_}, room_network{
                                                                   system.GetRoomNetwork()} {
+
+    const bool is_gamescope = UISettings::IsGamescope();
+    if (is_gamescope) {
+        setWindowFlags(Qt::Window | Qt::CustomizeWindowHint | Qt::WindowTitleHint);
+        setWindowModality(Qt::NonModal);
+
+        int w = 800;
+        int h = 500;
+        setFixedSize(w, h);
+    } else {
+        setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::WindowSystemMenuHint);
+    }
+
     ui->setupUi(this);
 
     // set up validation for all of the fields
